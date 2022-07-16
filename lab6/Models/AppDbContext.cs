83d5ea37d0cf423ba23cfe647259cd9d8dcf9239
sarisma.poudel <sarisma.poudel@dghub.io>
2022-07-16 23:26:50 +0545

    Add project files.

---
+++ lab6/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab6.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        { }
+        public DbSet<WebUser> WebUsers { get; set; }
+    }
+}
---
+++ lab6/Models/WebUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lab6.Models
+{
+    public class WebUser
+    {
+        [Column("UserId")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [StringLength(50)]
+
+        public string LastName { get; set; }
+        [StringLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
